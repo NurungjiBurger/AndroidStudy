59cbb76a78ceb80a25e530fd3f5bc7785d3b4bed
NurungjiBurger <qazw181900@naver.com>
2025-08-20 12:48:53 +0900

    Initial commit

---
+++ app/src/main/java/com/example/androidstudy/Main.java
@@ -0,0 +1,30 @@
+package com.example.androidstudy;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        int n = 50000; // 생성할 스레드 수
+        Thread[] threads = new Thread[n];
+
+        Runtime runtime = Runtime.getRuntime();
+        long startMemory = runtime.totalMemory() - runtime.freeMemory();
+
+        long startTime = System.currentTimeMillis();
+
+        for (int i = 0; i < n; i++) {
+            threads[i] = new Thread(() -> {
+                int sum = 0;
+                for (int j = 0; j < 10; j++) sum += j;
+            });
+            threads[i].start();
+        }
+
+        for (Thread t : threads) t.join();
+
+        long endTime = System.currentTimeMillis();
+        long endMemory = runtime.totalMemory() - runtime.freeMemory();
+
+        System.out.println("Thread Elapsed Time: " + (endTime - startTime) + " ms");
+        System.out.println("Memory Usage: " + (endMemory - startMemory) / 1024 + " KB");
+    }
+}
+
